a4cca122532028477ddd6ee5c7c79fe50ca1a760
diyakawayi <142962882+diyakawayi@users.noreply.github.com>
2023-10-19 14:56:44 +1000

    final

---
+++ MalaTang/AppDelegate.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    // Override point for customization after application launch.
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
